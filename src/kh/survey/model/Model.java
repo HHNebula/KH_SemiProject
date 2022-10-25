e674516bc19be3f475c1b811a3860ba590228f1c
yojulab <soqqhr@naver.com>
2022-10-25 15:39:46 +0900

    D

--- src/kh/survey/model/Model.java
+++ src/kh/survey/model/Model.java
@@ -20,20 +20,35 @@ public class Model {
         br.readLine();
     }
 
-    public void menu() { // 현정
+    public void menu()  { // 현정
 
-        // 메뉴 출력
+         // 메뉴 출력
         // A > 설문 시작
         // B > 전체 통계 조회
         // Z 종료
 
-    }
+
+}
+
+   public void verification(String loginID, String loginPW){
+//    -- +string verification(String loginId, String loginPw)
+//    메서드 호출해서 USER_ID 리턴
+//    만일 정보가 없다면 exception 발생 // 현정
+   
+   
+ 
+    
+   }
+    
 
     public void startSurvey() { // 성수
 
         // 설문 구현
+
         // 사용자한테 정보 입력 요구
+        
         // 유효검 검사 > 메서드 호출
+        
         // INSERT 메서드 호출
 
     }
