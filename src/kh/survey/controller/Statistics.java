e3cc68cdad6d97e3c42de0c605c097315c41346d
BoGyeong1 <wls6590@gmail.com>
2022-10-25 20:32:42 +0900

    통계 표시

--- src/kh/survey/controller/Statistics.java
+++ src/kh/survey/controller/Statistics.java
@@ -3,6 +3,7 @@ package kh.survey.controller;
 import java.sql.*;
 
 public class Statistics {
+
     public void statistics(Statement statement) {
 
 //최종목적
@@ -14,43 +15,34 @@ public class Statistics {
 // | 음식 만족도 |     n     |     n    |     n    |      n      |
 // | 시설 만족도 |     n     |     n    |     n    |      n      | 
 
-        
-// 갯수 구하는 쿼리
-// SELECT *
-// FROM (SELECT COUNT(ANSWER_ID) AS AS1
-// FROM user_reserv_qa
-// WHERE ANSWER_ID = 'AS1'AND QUESTION_ID = 'Q1') AS AS1
-// INNER JOIN (SELECT COUNT(ANSWER_ID) AS AS2
-// FROM user_reserv_qa
-// WHERE ANSWER_ID = 'AS2' AND QUESTION_ID = 'Q1' ) AS AS2
-// INNER JOIN (SELECT COUNT(ANSWER_ID) AS AS3
-// FROM user_reserv_qa
-// WHERE ANSWER_ID = 'AS3' AND QUESTION_ID = 'Q1' ) AS AS3
-// INNER JOIN (SELECT COUNT(ANSWER_ID) AS AS4
-// FROM user_reserv_qa
-// WHERE ANSWER_ID = 'AS4' AND QUESTION_ID = 'Q1' ) AS AS4
-// ;
+
 
 // 갯수 구하는 쿼리
-        String query = "SELECT COUNT(ANSWER_ID) AS CNT "+
-        " FROM user_reserv_qa "+
-        "WHERE QUESTION_ID = ?"+
-        "GROUP BY ANSWER_ID;";
+        String query = "SELECT questions.QUESTION, COUNT(CASE WHEN ANSWER_ID = 'AS1' THEN 1 END ) AS '매우 불만',"+
+        "COUNT(CASE WHEN ANSWER_ID = 'AS2' THEN 1 END ) AS '불만' ," +
+        "COUNT(CASE WHEN ANSWER_ID = 'AS3' THEN 1 END ) AS '만족'," +
+        "COUNT(CASE WHEN ANSWER_ID = 'AS4' THEN 1 END ) AS '매우 만족'"+
+        "FROM user_reserv_qa"+
+        "INNER JOIN questions ON user_reserv_qa.QUESTION_ID = questions.QUESTION_ID"+
+        "GROUP BY user_reserv_qa.QUESTION_ID;";
 
         ResultSet resultset;
 
 
         // 통계 보여주는 부분
+        // while문으로 데이터가 없을때까지 추출
         try {
            resultset = statement.executeQuery(query);
             
-           System.out.println("|     구분     | 매우불만 | 불만 | 만족 | 매우만족 |");
+           System.out.println("|     구분     | 매우 불만 |  불만  |  만족  | 매우 만족 |");
            while(resultset.next()){
-           System.out.print(" 접근 편리성 " );
-           System.out.print(" 직원 친절도 " );
-           System.out.print(" 호텔 청결도 " );
-           System.out.print(" 음식 만족도 " );
-           System.out.print(" 시설 만족도 " );
+            String Question = resultset.getString("QUESTION");
+            String AS1 = resultset.getString("매우 불만");
+            String AS2 = resultset.getString("불만");
+            String AS3 = resultset.getString("만족");
+            String AS4 = resultset.getString("매우 만족");
+            System.out.print( Question + " : " + AS1 + AS2 + AS3 + AS4 + "\n" );
+
            
         }
 
@@ -61,6 +53,4 @@ public class Statistics {
 
     }
 
-
-    
 }
