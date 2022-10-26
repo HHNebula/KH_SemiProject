581d24f538e60cf7446adb5f43c05717d6ff7c13
HHNebula <sungsoomr@icloud.com>
2022-10-26 09:28:13 +0900

    [SOO] Answer Updates

--- src/kh/survey/controller/Controller.java
+++ src/kh/survey/controller/Controller.java
@@ -7,9 +7,9 @@ public class Controller {
 	public void insertSurvey(Statement statement, String reservId, int answer1, int answer2, int answer3, int answer4,
 			int answer5) {
 
-		String query = "INSERT INTO user_reserv_qa VALUES ( '" + reservId + "', 'Q1', " + answer1 + " ), ( '" + reservId
-				+ "', 'Q2', " + answer2 + " ), ( '" + reservId + "', 'Q3', " + answer3 + " ), ( '" + reservId
-				+ "', 'Q4', " + answer4 + " ), ( '" + reservId + "', 'Q5', " + answer5 + " );";
+		String query = "INSERT INTO user_reserv_qa VALUES ( '" + reservId + "', 'Q1', 'AS" + answer1 + "' ), ( '" + reservId
+				+ "', 'Q2', 'AS" + answer2 + "' ), ( '" + reservId + "', 'Q3', 'AS" + answer3 + "' ), ( '" + reservId
+				+ "', 'Q4', 'AS" + answer4 + "' ), ( '" + reservId + "', 'Q5', 'AS" + answer5 + "' );";
 
 		try {
 			statement.executeQuery(query);
